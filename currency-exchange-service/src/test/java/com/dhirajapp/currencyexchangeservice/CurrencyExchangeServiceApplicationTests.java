c3225c146795d5e43d0d03caea1f131e7c117f80
dhirajapp <dhirajkumar31298@gmail.com>
2023-08-26 22:02:50 +0530

    currency exchange service setup

---
+++ currency-exchange-service/src/test/java/com/dhirajapp/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhirajapp.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
