d70c1f5b5041daf1886217b6301a45579d5d040c
dhirajapp <dhirajkumar31298@gmail.com>
2023-08-29 00:17:32 +0530

    implement feign rest client for service invocation

--- currency-conversion-service/src/main/java/com/dhirajapp/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/dhirajapp/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,16 +3,19 @@ package com.dhirajapp.currencyconversionservice.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
+import com.dhirajapp.currencyconversionservice.CurrencyExchangeProxy;
 import com.dhirajapp.currencyconversionservice.bean.CurrencyConversion;
 
 @RestController
 public class CurrencyConversionController {
+	@Autowired
+	private CurrencyExchangeProxy proxy;
    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity) {
 		
@@ -25,4 +28,14 @@ public class CurrencyConversionController {
 	return new CurrencyConversion(currencyConversion.getId(),currencyConversion.getFrom(),currencyConversion.getTo(),quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment());
 	}
 
+   
+   
+   @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity) {
+		
+	    CurrencyConversion currencyConversion=proxy.reteriveExchangeValueFeign(from, to);
+	return new CurrencyConversion(currencyConversion.getId(),currencyConversion.getFrom(),currencyConversion.getTo(),quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment());
+	}
+   
+   
 }
