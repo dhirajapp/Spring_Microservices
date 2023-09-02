0cb02863eb2a195b267c125febbb889b57de4931
dhirajapp <dhirajkumar31298@gmail.com>
2023-09-02 21:35:25 +0530

    setup nameing server

---
+++ nameing-server/src/test/java/com/dhirajapp/nameingserver/NameingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhirajapp.nameingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
