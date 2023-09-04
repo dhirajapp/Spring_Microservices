479a17d9dbbe18da365d6d23b109a268e62afd5a
dhirajapp <dhirajkumar31298@gmail.com>
2023-09-04 21:43:24 +0530

    setting up spring cloud api gateway

--- currency-conversion-service/src/main/java/com/dhirajapp/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/dhirajapp/currencyconversionservice/CurrencyExchangeProxy.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.dhirajapp.currencyconversionservice.bean.CurrencyConversion;
 
 //@FeignClient(name="currency-exchange",url="localhost:8000")
+
+// load balancer
+//@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
